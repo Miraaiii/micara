7dd17c31bb78f0364a60cd84738a52f65641683a
Miraaiii <doblados.marco@gmail.com>
2025-09-22 21:15:47 +0800

    a little css

--- login_reg.php
+++ login_reg.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN - REGISTER</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
 <body>
    <div class="container">
-        <div class="from-box login">
+
+        <!-- LOGIN -->
+        <div class="form-box login">
             <form action="#" method="post">
                 <h1>LOGIN</h1>
                 <div class="input-box">
@@ -23,18 +25,35 @@
                     <i class="fa-solid fa-envelope"></i>
                 </div>
 
-                <div class="forget-pass">
-                    <a href="#">Forget Password</a>
+                <div class="forgot-pass">
+                    <a href="#">Forgot Password?</a>
                 </div>
+                <button type="submit" class="btn">Login</button>
             </form>
         </div>
-        <div class="form-box register"></div>
+
+        <!-- REGISTER -->
+        <div class="form-box register">
+            <form action="#" method="post">
+                <h1>REGISTER</h1>
+                <div class="input-box">
+                    <input type="email" name="#" id="#" placeholder="Email" required>
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+
+                <div class="input-box">
+                    <input type="password" name="#" id="#" placeholder="Password" required>
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <button type="submit" class="btn">Register</button>
+            </form>
+        </div>
 
         <div class="toggle-box">
             <div class="toggle-panel toggle-left">
                 <h1>Hello Welcome!</h1>
                 <p>Don't have an account?</p>
-                <button class="btn register-btn"></button>
+                <button class="btn register-btn">Register</button>
             </div>
             <div class="toggle-panel toggle-right">
                 <h1>Welcome Back!</h1>
@@ -44,5 +63,5 @@
         </div>
    </div> 
 </body>
-<script src="script.js"></script>
+<script src="js/script.js"></script>
 </html>
